8c6d9027c74a8cf4d68a9dae317b132c00761493
Esteban Marin <estebanmarin002@gmail.com>
2023-03-07 18:27:26 -0500

    add

--- init.el
+++ init.el
@@ -1,9 +1,37 @@
+;; basic setup
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (set-fringe-mode 10)
 (setq ring-bell-function 'ignore)
+;; font setting
 (set-face-attribute 'default nil
                     :family "Iosevka"
                     :height 200)
+;; theme
+(load-theme 'tango-dark)
+
+;; make ESC quit promts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;;initialize package sources
+(require `package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" .  "https://orgmode.org/elpa")
+			 ("elpa"  . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; initialize use-package on non linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package ivy
+  :diminish
+  :config
+  (ivy-mode 1))
